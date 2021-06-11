b61bf656cebb32aaac3ed2d64de865f2ef61dc2c
GPL2020 <gplatta@gmail.com>
2021-06-11 20:18:13 -0300

    modified - puntajes_csv.py

--- TP1_Persistencia_en_Archivos_PrimeraParte/puntajes_csv.py
+++ TP1_Persistencia_en_Archivos_PrimeraParte/puntajes_csv.py
@@ -34,3 +34,9 @@ def recuperar_puntajes(nombre_archivo):
         puntajes.append((nombre, int(puntaje), tiempo))
     archivo.close()
     return puntajes
+
+import puntajes_csv                                                     
+valores = [("Pepe", 108, "4:16"), ("Juana", 2315, "8:42")]
+puntajes_csv.guardar_puntajes("puntajes.csv", valores)
+recuperado = puntajes_csv.recuperar_puntajes("puntajes.txt")
+print recuperado
